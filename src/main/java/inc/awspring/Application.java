9efe853e7747bb39c3c64e4846a3aaf41f61ee37
newhar <gtqh2yj@gmail.com>
2021-03-02 10:20:15 +0900

    Ch3 - Post API part 2

--- src/main/java/inc/awspring/Application.java
+++ src/main/java/inc/awspring/Application.java
@@ -3,7 +3,9 @@ package inc.awspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
